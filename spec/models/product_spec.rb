eefd15b9e9665e0126d608c2176b59060af0027e
JasonY1 <jasonh.yi@gmail.com>
2015-02-12 21:48:42 -0800

    ProductSpec: Validation tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validation" do
+    it { is_expected.to validate_presence_of :name }
+    it { is_expected.to validate_presence_of :type }
+    it { is_expected.to validate_presence_of :quantity }
+    it { expect(video).to be_valid }
+  end
 end
